e8aded167cae84b9e282337c89a1fea6d4b03767
tbechtoldbcs <32902910+tbechtoldbcs@users.noreply.github.com>
2017-10-29 10:21:13 +0100

    Add files via upload

---
+++ src/OeffnungszeitenIF.java
@@ -0,0 +1,7 @@
+package se_03;
+
+public interface OeffnungszeitenIF {
+	public void setOeffnungszeit(int oeffnen, int schliessen);
+	public int getOeffnungszeit();
+	public int getSchliessungszeit();
+}
---
+++ src/PreisCalculatorIF.java
@@ -0,0 +1,6 @@
+package se_03;
+
+public interface PreisCalculatorIF{
+	public double berechnePreis(int stundenzahl);
+	public double setStundensatz(double stundensatz);
+}
