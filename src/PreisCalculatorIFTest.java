d34bfa0e554b88d7c628d6f293437c1410793326
tbechtoldbcs <32902910+tbechtoldbcs@users.noreply.github.com>
2017-10-29 10:55:05 +0100

    Add files via upload

---
+++ src/PreisCalculatorIFTest.java
@@ -0,0 +1,39 @@
+package se_03;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PreisCalculatorIFTest {
+    private PreisCalculator simple, complex, custom;
+
+    @Before
+    public void setUp(){
+        //TODO create PreisCalculator objects
+    	//simple.stundensatz = 1d;
+    	//complex.stundensatz = 2.33d;
+    	//custom.stundensatz = 2d;
+    }
+
+    @Test
+    public void berechnePreis_simple_ReturnsExpectedValue(){
+    	assertEquals(5d, simple.berechnePreis(5));
+    }
+    
+    @Test
+    public void berechnePreis_zeroHours_ReturnsExpectedValue(){
+    	assertEquals(0, simple.berechnePreis(0));
+    }
+    
+    @Test
+    public void berechnePreis_complex_ReturnsExpectedValue(){
+    	assertEquals(18.64d, complex.berechnePreis(8));
+    }
+    
+    @Test
+    public void setStundensatz_customStundensatz_ReturnsExpectedValue(){
+    	custom.setStundensatz(1d);
+    	assertEquals(5d, simple.berechnePreis(5));
+    }
+}
