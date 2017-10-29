bd8f36d3d178aad398a6669f0f9f934398dde966
tbechtoldbcs <32902910+tbechtoldbcs@users.noreply.github.com>
2017-10-29 11:16:28 +0100

    Add files via upload

---
+++ src/FreiePlaetzeIFTest.java
@@ -0,0 +1,25 @@
+package se_03;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FreiePlaetzeIFTest {
+    private FreiePlaetzeIF leer, voll;
+
+    @Before
+    public void setUp(){
+        //TODO create FreiePlaetze objects
+    }
+
+    @Test
+    public void plaetzeFrei_leer_ReturnsTrue(){
+    	assertTrue(leer.plaetzeFrei());
+    }
+    
+    @Test
+    public void plaetzeFrei_voll_ReturnsFalse(){
+    	assertFalse(voll.plaetzeFrei());
+    }
+}
